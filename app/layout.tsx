c365b4c92d30c59734196006eb3110d615c9d564
Hagai Raja <hagairaja@telepix.net>
2024-09-17 01:19:53 +0100

    update og description

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,11 @@ export const metadata: Metadata = {
   description: 'This is Hagai Raja Sinulingga portfolio.',
   openGraph: {
     title: 'Hagai Raja Sinulingga Portfolio',
-    description: 'This is Hagi\'s portfolio.',
+    description: `Hi there! I’m Hagai, an AI researcher and engineer originally from Indonesia, with a penchant for solving complex problems using cutting-edge algorithms. Armed with a master’s degree from Sejong University (4.5 GPA, I’m serious!), I’ve spent the last few years developing AI solutions for satellite imagery and remote sensing, making sure our planet is mapped better than ever!
+
+My expertise spans object detection, few-shot segmentation, and even super-resolution—think of it as making low-res images magically clear! Alongside AI, I’ve dabbled in finance, startups, and politics. Also, did I mention I love tackling AI challenges? From ESA to KARI, I’m always testing new algorithms in global competitions.
+
+When I’m not glued to my screen, you’ll find me running through the city’s parks. It’s my all-in-one: staying fit, meeting new people, and sparking fresh ideas for my next AI project. I'll post it here, so stay tuned!`,
     url: baseUrl,
     siteName: 'hagairaja.github.io',
     locale: 'en_US',
